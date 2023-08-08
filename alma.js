fa0224b3bf653c180b85cfebd5e7aacc670b4dd9
thompa21 <tholind@kth.se>
2023-08-08 15:43:43 +0200

    fixes

--- alma.js
+++ alma.js
@@ -143,7 +143,7 @@ jsonuser += `,
                     "user_viewable": false,
                     "popup_note": true,
                     "created_by": "kthb_forms",
-                    "created_date": "${new Date().toLocaleString()}Z",
+                    "created_date": "${new Date().toLocaleDateString()}Z",
                     "note_owner": ""
                 }
             ]`
